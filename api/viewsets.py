a1a5bded879d3997c2a37443dbcbb39ac8e2c3ca
yasinkbas <yasin.kbas12@gmail.com>
2019-08-26 00:05:42 +0300

    modified auth

--- api/viewsets.py
+++ api/viewsets.py
@@ -23,7 +23,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticated,)
     filterset_class = MovieFilter #5
 
     @action(methods=['get'], detail=False)  
