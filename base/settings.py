1c5ac26427dc3b263f8a84fd60e5cd8b18962a14
yasinkbas <yasin.kbas12@gmail.com>
2019-08-26 00:34:00 +0300

    removed some old applications

--- base/settings.py
+++ base/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
-    'allauth.socialaccount',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
