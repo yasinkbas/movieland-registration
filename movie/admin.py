297785f8c9ad711b9fc4e0862b510ac1f809483c
yasinkbas <yasin.kbas12@gmail.com>
2019-08-28 23:59:20 +0300

    customized movie on admin panel

--- movie/admin.py
+++ movie/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'imdb','isPublish','director_name','user']
+    list_display_links = ('id', 'title')
+
+admin.site.register(models.Movie,MovieAdmin)
 # admin.site.register(models.Director)
